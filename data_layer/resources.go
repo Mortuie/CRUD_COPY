eaee0bf64d424e901e574b84f37c120d5b75969d
mortuie <jimmybob-leon@hotmail.co.uk>
2023-03-07 22:37:46 +0000

    Added POST & GET {id} routes

--- data_layer/resources.go
+++ data_layer/resources.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 
 	"github.com/mortuie/CRUD_COPY/constants"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -14,19 +16,50 @@ type ResourceModel struct {
 	DB *mongo.Client
 }
 
-func (m ResourceModel) InsertResource() error {
+func (m ResourceModel) GetResource(collectionName string, id string) (map[string]interface{}, error) {
+	db := m.DB.Database(constants.RESOURCE_DB)
+	collection := db.Collection(collectionName)
 
-	return errors.New("memes")
+	objectId, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		fmt.Println("Invalid resource id", id)
+		return nil, errors.New("Invalid resource id: " + id)
+	}
+
+	res := collection.FindOne(context.TODO(), bson.M{"_id": objectId})
+	anyJson := map[string]interface{}{}
+	res.Decode(anyJson)
+	return anyJson, nil
 }
 
-func (m ResourceModel) CreateCollection(name string) error {
+func (m ResourceModel) InsertResource(collectionName string, resource map[string]interface{}) (map[string]interface{}, error) {
+	db := m.DB.Database(constants.RESOURCE_DB)
+	collection := db.Collection(collectionName)
+
+	b, err := collection.InsertOne(context.TODO(), resource)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("ID: ", b.InsertedID.(primitive.ObjectID).Hex())
+
+	idString := b.InsertedID.(primitive.ObjectID).Hex()
+
+	resource["_id"] = idString
+
+	return resource, nil
+}
+
+func (m ResourceModel) CreateCollection(collectionName string) error {
 	db := m.DB.Database(constants.RESOURCE_DB)
 
-	err := db.CreateCollection(context.TODO(), name)
+	err := db.CreateCollection(context.TODO(), collectionName)
 
 	if err != nil {
 		if strings.Contains(err.Error(), "Collection already exists.") {
-			fmt.Printf("Collection: %s already exists.\n", name)
+			fmt.Printf("Collection: %s already exists.\n", collectionName)
 		} else {
 			fmt.Println("ERROR CREATING COLLECTION", err)
 			return err
--- routes/resource_routes.go
+++ routes/resource_routes.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/google/uuid"
 	"github.com/mortuie/CRUD_COPY/data_layer"
 	"github.com/mortuie/CRUD_COPY/middleware"
 	"github.com/mortuie/CRUD_COPY/models"
@@ -21,15 +20,46 @@ func (deps ResourceRoutesDeps) ResourceRoutes() chi.Router {
 
 	r.Use(middleware.Resource_request_param_validation)
 	r.Get("/", deps.list)
+	r.Get("/{resourceId}", deps.get_one)
 	r.Post("/", deps.create)
 
 	return r
 }
 
+func write_err_response(w http.ResponseWriter, message string, statusCode int) {
+	b, _ := json.Marshal(models.ErrorResponse{Message: message})
+	w.WriteHeader(statusCode)
+	w.Write(b)
+}
+
+func (deps ResourceRoutesDeps) get_one(w http.ResponseWriter, r *http.Request) {
+	rp := r.Context().Value("request_params").(models.RequestParams)
+	resourceId := chi.URLParam(r, "resourceId")
+
+	if resourceId == "" {
+		write_err_response(w, "ResourceId not valid", 400)
+		return
+	}
+
+	res, err := deps.Resources.GetResource(rp.Uuid, resourceId)
+
+	if err != nil {
+		write_err_response(w, err.Error(), 400)
+		return
+	}
+
+	if len(res) == 0 {
+		write_err_response(w, "Resource not found", 404)
+		return
+	}
+
+	b, _ := json.Marshal(res)
+	w.WriteHeader(200)
+	w.Write(b)
+}
+
 func (deps ResourceRoutesDeps) list(w http.ResponseWriter, r *http.Request) {
 	rp := r.Context().Value("request_params").(models.RequestParams)
-
-	deps.Resources.CreateCollection("memes")
 
 	fmt.Printf("%+v\n", rp)
 
@@ -38,21 +68,26 @@ func (deps ResourceRoutesDeps) list(w http.ResponseWriter, r *http.Request) {
 
 func (deps ResourceRoutesDeps) create(w http.ResponseWriter, r *http.Request) {
 	rp := r.Context().Value("request_params").(models.RequestParams)
-
-	_ = rp
+	deps.Resources.CreateCollection(rp.Uuid)
 
 	var anyJson map[string]interface{}
 
 	json.NewDecoder(r.Body).Decode(&anyJson)
+	delete(anyJson, "_id")
 
 	if anyJson == nil {
-		b, _ := json.Marshal(models.ErrorResponse{Message: "JSON body is empty."})
-		w.WriteHeader(400)
-		w.Write(b)
+		write_err_response(w, "JSON body is empty.", 400)
 		return
 	}
 
-	anyJson["id"] = uuid.New().String()
+	res, err := deps.Resources.InsertResource(rp.Uuid, anyJson)
 
-	w.Write([]byte("creating..."))
+	if err != nil {
+		write_err_response(w, "Internal server error", 500)
+		return
+	}
+
+	b, _ := json.Marshal(res)
+	w.WriteHeader(200)
+	w.Write(b)
 }
